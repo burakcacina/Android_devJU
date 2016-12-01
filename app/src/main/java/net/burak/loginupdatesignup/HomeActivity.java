d4a3dcc3535b6f58bd8f8b2d2597b68f52f8fcef
farah <farah_iraqi@hotmail.com>
2016-12-02 00:17:42 +0100

    Adding Advertisement, browse page with fragment loading

--- app/src/main/java/net/burak/loginupdatesignup/HomeActivity.java
+++ app/src/main/java/net/burak/loginupdatesignup/HomeActivity.java
@@ -6,12 +6,32 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import net.mijlk.service.Recipe;
+import net.mijlk.service.RecipeRestService;
+
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class HomeActivity extends BaseActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecipeListAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Recipe> recipeList = new ArrayList<>();
+    private RecipeRestService recipeRestService = new RecipeRestService();
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private boolean loadingDataCompleted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +48,60 @@ public class HomeActivity extends BaseActivity {
             String json = bundle.getString("access_token");
             System.out.println(json);
         }
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recipeList);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Retain an instance so that you can call `resetState()` for fresh searches
+        scrollListener = new EndlessRecyclerViewScrollListener(mLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                if(!loadingDataCompleted){
+                    loadRecipesList(page);
+                }
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        mRecyclerView.addOnScrollListener(scrollListener);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecipeListAdapter(this, recipeList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        loadRecipesList(1);
+
     }//TEST
 
+    private void loadRecipesList(int pageNum){
+        Call<List<Recipe>> call = recipeRestService.getRecipes(pageNum);
+        call.enqueue(new Callback<List<Recipe>>() {
+            @Override
+            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                int statusCode = response.code();
+                List<Recipe> recipesList = response.body();
+                if(recipesList.size() == 0){
+                    loadingDataCompleted = true;
+                    scrollListener.setLastPage(true);
+                }else{
+                    mAdapter.updateRecipeList(recipesList);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                // Log error here since request failed
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
---
+++ app/src/main/java/net/mijlk/service/RecipeRestService.java
@@ -0,0 +1,43 @@
+package net.mijlk.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Farah on 25-11-2016.
+ */
+
+public class RecipeRestService {
+
+    private static final String BASE_URL = "http://52.211.99.140/api/v1/";
+
+    private Retrofit retrofit;
+
+    private Gson gson;
+
+    private RecipeApiEndPointInterface recipeApi;
+
+    public RecipeRestService(){
+        gson = new GsonBuilder()
+//                .setDateFormat("dd.MM.yyyy")
+                .create();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        recipeApi = retrofit.create(RecipeApiEndPointInterface.class);
+    }
+
+    public Call<List<Recipe>> getRecipes(int pageNum){
+        return recipeApi.getRecipes(pageNum);
+    }
+
+
+}
